d1434261917a9d9f0530d701894a265d0a8291a4
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-12-06 20:17:11 +0530

    Create PrintinUpto100.cs

---
+++ PrintinUpto100.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int n=100;
+        for(int i=1;i<=n;i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
