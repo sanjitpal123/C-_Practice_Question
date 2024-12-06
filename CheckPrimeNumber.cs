0fa5ef6bd6038fbfb875f891b21085d0e8542e18
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-12-06 21:23:13 +0530

    Create CheckPrimeNumber.cs

---
+++ CheckPrimeNumber.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+     int c=0;
+     Console.WriteLine("Enter a number");
+     int n = int.Parse(Console.ReadLine());
+     for(int i=1;i<=n;i++)
+     {
+         if(n%i==0)
+         {
+             c++;
+         }
+     }
+     if(c==2)
+     {
+         Console.WriteLine("Prime number");
+     }
+     else
+    {
+        Console.WriteLine("Not Prime Number");
+    }
+     
+    }
+}
