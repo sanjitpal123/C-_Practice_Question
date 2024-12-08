a13e90d81d10dea2276ac433964f56e5ea4d071c
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-12-08 22:21:42 +0530

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+
+
+using System;
+
+public class calculator{
+    public void add(int a, int b)
+    {
+        Console.WriteLine(a+b);
+    }
+    public void substact (int a, int b)
+    {
+        Console.WriteLine(a-b);
+    }
+    public void multiply(int a, int b)
+    {
+        Console.WriteLine(a*b);
+    }
+}
+
+public class Mainclass{
+    public static void Main(string[] args)
+    {
+        calculator c=new calculator();
+        c.add(10,20);
+        c.substact(30,10);
+        c.multiply(40,10);
+    }
+}
